4389ba65d5c9cce556f5c01b37187779b59be1ea
Geovane Leandro da Costa Junior <geovanedesenha@gmail.com>
2018-08-02 10:46:24 -0400

    Initial Commit

---
+++ NaLenha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaLenha
+//
+//  Created by Geovane Leandro da Costa Junior on 02/08/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
